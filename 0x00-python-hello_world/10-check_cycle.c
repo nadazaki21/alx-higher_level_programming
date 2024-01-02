38add6abf556caf7bb5b7d0b6dbaaa13d3e47c8a
nadazkai21 <nadazaki019@gmail.com>
2024-01-02 02:10:38 +0200

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,23 +4,22 @@
  * check_cycle - checks is linked list is cylic
  * @list: the linked list to check
  * Return: 0 or 1 to indicate if the linked list is cylic or not
-*/
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *ptr1, *ptr2;
 
 	if (list == NULL || list->next == NULL)
 		return (0);
-	
+
 	ptr1 = list->next->next; /* moves 2 stepps*/
-	ptr2 = list->next; /* moves 1 step s*/
+	ptr2 = list->next;		 /* moves 1 step s*/
 
 	while (ptr1 && ptr2 && ptr1->next)
 	{
-		if (ptr1 == ptr2 )
+		if (ptr1 == ptr2)
 			return (1);
 		ptr1 = ptr1->next->next;
 		ptr2 = ptr2->next;
-		
 	}
 }
