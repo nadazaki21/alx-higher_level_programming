2e01f9260b8253f86982fd6cc8499c5e9db26a53
nadazkai21 <nadazaki019@gmail.com>
2024-01-08 01:17:12 +0200

    task5 modified

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 def no_c(my_string):
-    if my_string:
-        new_string = ""
-        for i in my_string:
-            if (i != 'c') and (i != 'C'):
-                new_string = new_string + i
-        return (new_string)
+    length = len(my_string)
+    i = 0
+    while i < length:
+        if (my_string[i] == 'c') or (my_string[i] == 'C'):
+            if i == 0:
+                my_string = my_string[1:]
+            elif i < (length-1):
+                first_half = my_string[:i]
+                second_half = my_string[i+1:]
+                my_string = first_half + second_half
+            elif i == (length-1):
+                my_string = my_string[:-1]
+        i += 1
+        length = len(my_string)
+    return (my_string)
