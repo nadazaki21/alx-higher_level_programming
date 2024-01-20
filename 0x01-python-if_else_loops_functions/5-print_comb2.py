75d4fcb5c4b340b3f654562b80d4ac8b538d46ef
nadazkai21 <nadazaki019@gmail.com>
2024-01-20 21:16:09 +0200

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if (i < 10):
-        print("0{:d}, ".format(i), end="")
-    else:
-        print("{:d}".format(i), end="")
-        if i != 99:
-            print(", ", end="")
+for i in range(100):
+    print("{:02d}".format(i), end="")
+    if (i != 99):
+        print(", ", end="")
