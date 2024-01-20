a1ae030d9eba8955c0937fbdc1776253e066eb09
nadazkai21 <nadazaki019@gmail.com>
2024-01-20 23:45:59 +0200

    task13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+/**
+ * 
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *cursor = *head, *new_node, *prev_node, *next_node; 
+    
+    if (cursor == NULL)
+        return (NULL);
+    
+    while (cursor != NULL)
+    {
+        if ((cursor->n < number) && (cursor->next->n > number))
+        {
+            prev_node = cursor;
+            next_node = cursor->next;
+            new_node = malloc(sizeof(listint_t));
+            if (new_node == NULL)
+                return (NULL);
+            new_node->n = number;
+            new_node->next = next_node;
+            prev_node->next = new_node;
+            break;
+        }
+        else if ((cursor->n < number) && (cursor->next == NULL))
+        {
+            prev_node = cursor;
+
+            new_node = malloc(sizeof(listint_t));
+            if (new_node == NULL)
+                return (NULL);
+
+            new_node->n = number;
+            prev_node->next = new_node;
+            new_node->next = NULL;
+            break;
+        }
+        else if ((cursor->n > number) && ((cursor->next == NULL) || (cursor->next != NULL)))
+        {
+            /* if is it the whole list is biger than then number */
+            new_node = malloc(sizeof(listint_t));
+            if (new_node == NULL)
+                return (NULL);
+
+            new_node->n = number;
+            new_node->next = cursor;
+            break;
+        }
+        cursor = cursor->next;
+    }
+    return (new_node);
+}
