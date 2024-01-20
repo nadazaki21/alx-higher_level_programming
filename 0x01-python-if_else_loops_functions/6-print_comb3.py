918c24f2049fc90edbd6e62ddb6ea7ebe563219f
nadazkai21 <nadazaki019@gmail.com>
2024-01-20 21:47:35 +0200

    task6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(((i*11)+1),((i*10)+10)):
+        if (j != 89):
+            print("{:02d}".format(j),end=", ")
+        else:
+            print("{:02d}\n".format(j),end="")
