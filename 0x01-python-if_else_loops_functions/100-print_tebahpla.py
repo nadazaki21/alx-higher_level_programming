ed5309b22229e65d7897017eedeee588ef967541
nadazkai21 <nadazaki019@gmail.com>
2024-01-21 00:32:40 +0200

    task100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 flag = 1
 for i in range(122, 96, -1):
-    print("{}".format(chr(i)) if flag else "{}".format(chr(i-32)) ,end="")
+    print("{}".format(chr(i)) if flag else "{}".format(chr(i-32)), end="")
     flag = not flag
