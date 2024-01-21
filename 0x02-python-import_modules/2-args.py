4d5395a21892f068ca39d22795bdeda0a86e5c0d
nadazkai21 <nadazaki019@gmail.com>
2024-01-21 02:13:49 +0200

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(len(argv)))
-        for i in range(len(argv)):
+        for i in range(1,len(argv)):
             print("{}: {}".format(i, argv[i]))
