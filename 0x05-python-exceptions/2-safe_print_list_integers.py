88a6d15c911efc5ce92a129b68378cb6ce59796c
nadazkai21 <nadazaki019@gmail.com>
2024-01-23 00:46:33 +0200

    2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 integer_count += 1
         print()
     except IndexError:
-        print()
         pass
     finally:
         return integer_count
