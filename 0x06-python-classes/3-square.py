cc99bb45bdbcd65b19fa78749c212917499b33b6
nadazkai21 <nadazaki019@gmail.com>
2024-01-23 18:43:21 +0200

    3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square():
+    """ class that defines a square """
+    def __init__(self, size=0):
+        if (isinstance(size, int) != 1):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return (size*size)
