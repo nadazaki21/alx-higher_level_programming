7bee92a0bd26ef482b05f8b70a55abcaa6165ed7
nadazkai21 <nadazaki019@gmail.com>
2024-01-28 15:24:44 +0200

    00

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-""" task 0 """
+"""
+task 0
+adding 2 inetegers
+numbers in strings are not converted to inetgers
+floats are converted to inetegers
+"""
+
 
 def add_integer(a, b=98):
     """ adds 2 integers """
-    if  not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     sum = int(a) + int(b)
 
     return sum
-
