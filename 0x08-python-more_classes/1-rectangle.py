cbcfa5583f2cb328c80a0603c5dba9e3d8cc6c8e
nadazkai21 <nadazaki019@gmail.com>
2024-01-29 18:35:52 +0200

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,18 +5,19 @@ class Rectangle that defines a rectangle
 
 
 class Rectangle():
-    """ 
-    defines a rectangle
-    with a width and a height 
     """
-    
+    defines a rectangle
+    with a width and a height
+    """
+
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
-    
+        self.__width = width
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -24,10 +25,11 @@ class Rectangle():
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
