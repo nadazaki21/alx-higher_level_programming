59437d51739f4265a1e3fa337433836871ae87da
nadazkai21 <nadazaki019@gmail.com>
2024-01-29 19:22:22 +0200

    1

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,7 +38,7 @@ class Rectangle:
         self.__height = value
         
     def area(self):
-        return self.__width * self.__height
+        return self.width * self.height
     
     def perimeter(self):
-        return 2 * (self.__height + self.width)
+        return 2 * (self.height + self.width)
