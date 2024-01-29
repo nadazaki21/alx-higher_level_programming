ddd4b8cafe490d2f54074e2704dbdf0adda516a9
nadazkai21 <nadazaki019@gmail.com>
2024-01-29 18:55:13 +0200

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,10 +9,9 @@ Default values of both attributes are 0.
 
 class Rectangle:
     """ defines a rectangle with a width and a height """
-
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
