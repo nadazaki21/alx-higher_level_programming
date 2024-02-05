67747ba4321c468f09056757c0509a580e5b7873
nadazkai21 <nadazaki019@gmail.com>
2024-02-05 21:32:11 +0200

    4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""function that returns True if the object is an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False. """
+"""
+function that returns True if the object is an instance
+of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False.
+"""
 
 
 def inherits_from(obj, a_class):
     """ check if object belons to inheritrance chain """
-    if isinstance(obj, a_class):
+    obj_class = type(obj)
+    if issubclass(obj_class, a_class):
+        if obj_class is a_class:
+            return False
         return True
     else:
         return False
