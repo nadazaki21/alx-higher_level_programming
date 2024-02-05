b2e623d12902b9169fe7945ea0581f0723888193
nadazkai21 <nadazaki019@gmail.com>
2024-02-05 23:26:02 +0200

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
+
 """
 ========================================
 BaseGGeomtry and Rectangle class and sqaure class
@@ -9,6 +10,7 @@ BaseGGeomtry and Rectangle class and sqaure class
 
 class Square(Rectangle):
     """ Sqaure class that inherits Rectangle """
+    
     def __init__(self, size):
         """ instilaizes saure size """
         super().__init__(size, size)
