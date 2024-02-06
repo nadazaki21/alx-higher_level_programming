838ae0c9870f51870bdc7145a38047f16cd37afa
nadazkai21 <nadazaki019@gmail.com>
2024-02-06 18:25:57 +0200

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" module 0 """
+""" module 1 """
 
 
-def read_file(filename=""):
-    """ function that reads a text file (UTF8) and prints it to stdout """
-    with open(filename, 'r') as file:
-        data = file.read
-        print(data)
+def append_write(filename="", text=""):
+    """ function that writes a string to a text file
+    (UTF8) and returns the number of characters written """
+    with open(filename, "a", encoding="UTF-8") as file:
+        start = file.tell()
+        file.write(text)
+        finish = file.tell()
+    return finish - start
