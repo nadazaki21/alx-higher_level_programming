2deddb8d8429469a37ab9833d4b07c8d26bec84a
nadazkai21 <nadazaki019@gmail.com>
2024-02-06 21:31:09 +0200

    8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module 8 """
+import json
+
+def class_to_json(obj):
+    """  function that returns the dictionary description
+    with simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object """
+    result = {}
+    for attr_name, attr_value in obj.__dict__.items():
+        if isinstance(attr_value, (list, dict, str, int, bool)):
+            result[attr_name] = attr_value
+    #return vars(obj)
+    return result
