66f1cef5bea0d22e7c27be464ad3a8662d47b275
nadazkai21 <nadazaki019@gmail.com>
2024-02-06 17:59:46 +0200

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """ function that print content of file """
     with open(filename, 'r', encoding='utf-8') as file:
         data = file.read()
         print(data)
