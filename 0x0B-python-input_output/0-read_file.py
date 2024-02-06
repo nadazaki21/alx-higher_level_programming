4b890314bf90afe9eedbfb3c4e46be34886cac13
nadazkai21 <nadazaki019@gmail.com>
2024-02-06 17:58:46 +0200

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end='')
+        data = file.read
+        print(data)
