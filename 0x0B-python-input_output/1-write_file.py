9c81dc05064caaad2a7d3753de3a1652786c4ec3
nadazkai21 <nadazaki019@gmail.com>
2024-02-06 18:23:13 +0200

    1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module 1 """
+
+
+def write_file(filename="", text=""):
+    """ function that writes a string to a text file (UTF8) and returns the number of characters written """
+    with open(filename, "w", encoding="UTF-8") as file:
+        start = file.tell()
+        file.write(text)
+        finish = file.tell()
+    return finish - start
