3a21e5ea0c0676d97b32b6e51afd57a9619ab1b7
nadazkai21 <nadazaki019@gmail.com>
2024-02-06 23:27:52 +0200

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Class Student """
+
+
+class Student():
+    """ class that represents a student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            if attrs.count != 0:
+                for i in attrs:
+                    if type(i) is not str:
+                        return vars(self)
+                valid_att = {}
+                for attr_name, attr_value in self.__dict__.items():
+                    for i in attrs:
+                        if i == attr_name:
+                            valid_att[attr_name] = attr_value
+                return valid_att
+            else:
+                return vars(self)
+        else:
+            return vars(self)
+
+            
