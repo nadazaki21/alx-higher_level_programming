a1e55231d90d5c5ecb22726b2f14206dd52ad2e3
nadazkai21 <nadazaki019@gmail.com>
2024-03-14 01:02:08 +0200

    6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- cript that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE table IF NOT EXISTS hbtn_0d_usa.states(id INT PRIMARY KEY, name VARCHAR(256) NOT NULL)
+CREATE table IF NOT EXISTS hbtn_0d_usa.states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)
