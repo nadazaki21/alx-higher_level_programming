b4577e58f981eafb72e871aa4a6daabd560755ab
nadazkai21 <nadazaki019@gmail.com>
2024-04-09 03:42:35 +0200

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (Number(process.argv[2]) === parseInt(process.argv[2])) {
+if (Math.floor(Number(process.argv[2])) === parseInt(process.argv[2])) {
   console.log('My number : ' + parseInt(process.argv[2]));
 } else {
   console.log('Not a number');
