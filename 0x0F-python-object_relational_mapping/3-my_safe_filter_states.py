b7278e5bcfbcb0ea8d98bebf6a877eaf31a07726
nadazkai21 <nadazaki019@gmail.com>
2024-04-13 14:51:46 +0200

    3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" same as task 2, but safe from sql injections """
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * \
+        FROM  states WHERE BINARY name='{}' \
+        ORDER BY id ASC".format(
+            sys.argv[4]
+        )
+    )
+
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
