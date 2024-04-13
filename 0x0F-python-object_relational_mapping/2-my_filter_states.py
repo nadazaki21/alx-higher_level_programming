7daacae9a973ed425f20f78d38f474b7743cacb6
nadazkai21 <nadazaki019@gmail.com>
2024-04-13 13:43:01 +0200

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,10 +2,9 @@
 """ script that takes in an argument and displays all
 values in the states table
 of hbtn_0e_0_usa where name matches the argument."""
-
-
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
