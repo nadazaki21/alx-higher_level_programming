679b3bcc9d1d108f11fbd1fa4cd759626ff2ddc9
nadazkai21 <nadazaki019@gmail.com>
2024-04-13 14:53:44 +0200

    3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cur.execute(
         "SELECT * \
         FROM  states WHERE BINARY name=%s \
-        ORDER BY id ASC",(sys.argv[4],)
+        ORDER BY id ASC", (sys.argv[4],)
     )
 
     result = cur.fetchall()
