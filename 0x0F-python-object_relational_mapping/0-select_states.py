f7cfa23bb06b6fa478893d9a5b41193a6a00df02
nadazkai21 <nadazaki019@gmail.com>
2024-04-13 13:19:25 +0200

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,15 +5,16 @@ import sys
 import MySQLdb
 
 
-db = MySQLdb.connect(
-    host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
-)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+    )
 
-cur = db.cursor()
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states")
 
-rows = cur.fetchall()
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
+    for row in rows:
+        print(row)
