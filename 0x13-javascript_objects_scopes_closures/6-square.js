d6fb201d199d05406c1a89d56c260d4186369ad9
nadazkai21 <nadazaki019@gmail.com>
2024-04-25 23:46:25 +0200

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ module.exports = class Square extends oldSquare {
       }
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('c'.repeat(this.width));
+        console.log('C'.repeat(this.width));
       }
     }
   }
