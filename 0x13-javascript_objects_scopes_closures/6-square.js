79761e4e17f4007d02d68e0f6ded569899a6350a
nadazkai21 <nadazaki019@gmail.com>
2024-04-25 23:45:35 +0200

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const oldSquare = require('./5-square');
+
+module.exports = class Square extends oldSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+};
