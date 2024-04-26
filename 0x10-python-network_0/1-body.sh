5b15445c3001e08d387bc712a3ccb23055851e4a
nadazkai21 <nadazaki019@gmail.com>
2024-04-26 22:42:42 +0300

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # prints the body of the repsonse of requests whose reposnse status is 200.
-RESPONSE=$(curl -sI $1 | grep -E " {1}[0-9]{3} {1}"  | cut -d " " -f 2);if [ $RESPONSE == "200" ]; then curl -sXL GET $1; fi
+RESPONSE=$(curl -sI $1 | grep -E " {1}[0-9]* {1}"  | cut -d " " -f 2);if [ $RESPONSE == "200" ]; then curl -sX GET $1; fi
