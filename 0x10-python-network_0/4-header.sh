f944b1db6dbdbe1578b551ad38652e06a0520d53
nadazkai21 <nadazaki019@gmail.com>
2024-04-26 23:34:31 +0300

    4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-
-curl -si -X get localhost
+# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
+curl -sIH "X-School-User-Id: 98" "$1"
