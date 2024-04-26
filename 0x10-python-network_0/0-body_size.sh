d7c408a1cbc7966680962af9f4753b9dd8edbc5f
nadazkai21 <nadazaki019@gmail.com>
2024-04-26 22:04:02 +0300

    1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#curl  -ws "%{size_download}"  "$1"
 curl -s --head $1 | grep Content-Length | cut -d " " -f 2
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+RESPONSE=$(curl -sI $1 | grep HTTP | cut -d " " -f 2); if [ $RESPONSE == "200" ]; then curl -s $1; fi
