1aed70b70710421e07d9f8c30109294717a4a95a
nadazkai21 <nadazaki019@gmail.com>
2024-04-27 21:32:49 +0300

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,29 +8,13 @@ import sys
 
 
 if __name__ == "__main__":
-    
-    
+
     url = sys.argv[1]
-    values = {'email': sys.argv[2]}
-    #email = sys.argv[2]
+    values = {"email": sys.argv[2]}
 
     data = urllib.parse.urlencode(values)
-    data = data.encode("utf-8") # data should be bytes
+    data = data.encode("utf-8")  # data should be bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-       the_page = response.read().decode("utf-8")
-       print(the_page)
-   
-    # url = sys.argv[1]
-    # email = sys.argv[2]
-
-    # # Prepare POST data as a byte string with proper encoding
-    # data = {"email": email}
-    # data = urllib.parse.urlencode(data).encode("utf-8")
-    # # Create a POST request with the data
-    # req = urllib.request.Request(url, data)
-
-
-    # with urllib.request.urlopen(req) as response:
-    #     the_page = response.read().decode("utf-8")
-    #     print(the_page)
+        the_page = response.read().decode("utf-8")
+        print(the_page)
