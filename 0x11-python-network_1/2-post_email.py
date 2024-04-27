5f2770d1c73bb4fc521e758a43e4b93be733a207
nadazkai21 <nadazaki019@gmail.com>
2024-04-27 21:27:59 +0300

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     
     
     url = sys.argv[1]
-    #values = {'email': sys.argv[2]}
-    email = sys.argv[2]
+    values = {'email': sys.argv[2]}
+    #email = sys.argv[2]
 
-    data = urllib.parse.urlencode(email)
+    data = urllib.parse.urlencode(values)
     data = data.encode('ascii') # data should be bytes
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
