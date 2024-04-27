0f8214edf16756cf7c86af51653df8124d32fbaf
nadazkai21 <nadazaki019@gmail.com>
2024-04-27 21:20:53 +0300

    2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 """ script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+
+url = "https://alx-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
-   html = response.read()
-   print ("- type: " + str(type(html)))
-   print ("- content: " + str(html))
-   print("- utf8 content: " + str(html.decode("utf-8")))
+    html = response.read()
+    print("- type: " + str(type(html)))
+    print("- content: " + str(html))
+    print("- utf8 content: " + str(html.decode("utf-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" script that takes in a URL, sends a request
+to the URL and displays the value of the X-Request-Id
+variable found in the header of the response. """
+
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+header_name = "X-Request-Id"
+
+with urllib.request.urlopen(url) as response:
+    headers = response.headers
+    if header_name in headers:
+        print(headers.get(header_name))
