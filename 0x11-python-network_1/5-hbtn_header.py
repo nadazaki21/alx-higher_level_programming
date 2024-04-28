5a3c3538bee3e984ceb4d0ce96e6de4e7314acb7
nadazkai21 <nadazaki019@gmail.com>
2024-04-28 20:44:12 +0300

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header """
+""" script that takes in a URL, sends a request
+to the URL and displays the value of the variable
+X-Request-Id in the response header """
 import requests
 import sys
 
@@ -8,4 +10,4 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
     r = requests.get(url)
-    r.headers["X-Request-Id"]
+    print(r.headers["X-Request-Id"])
