9fa047839faac400e88e05d18118cc9f6c3493ba
nadazkai21 <nadazaki019@gmail.com>
2024-04-29 01:47:56 +0300

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    # head = {'accept': 'application/vnd.github+json', }
+    cred = requests.auth.HTTPBasicAuth(username, password)
+
+    r = requests.get(f"https://api.github.com/users/{username}", auth=cred)
+    print(r.json().get("id"))
