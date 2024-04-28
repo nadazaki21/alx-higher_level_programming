74d77a0ab6004f01d004ed5ef55653f55781ce90
nadazkai21 <nadazaki019@gmail.com>
2024-04-28 22:53:32 +0300

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     try:
         r = requests.post("http://0.0.0.0:5000/search_user", params=payload)
         r.json()  # if json decoding fails , it raises an exception
-        print(r)
+        print(r.text)
             
     except requests.exceptions.JSONDecodeError:
         print("Not a valid JSON")
