452446264b264721caaa3891487b5d5fe2f4cd93
nadazkai21 <nadazaki019@gmail.com>
2024-04-28 22:31:48 +0300

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" script that takes in a URL, sends a request
+to the URL and displays the body of the response. """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+
+    try:
+        r = requests.get(url)
+        if r.status_code >= 400:
+            print("Error code: ", end="")
+        else:
+            print(r.text)
+    except Exception:
+        pass
