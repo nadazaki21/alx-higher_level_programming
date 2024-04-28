58ca83b0b4d0c5662c4bd43535739c512a88817b
nadazkai21 <nadazaki019@gmail.com>
2024-04-28 20:38:34 +0300

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+
+if __name__ == "__main__":
+
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: ", end="")
+    print(type(r.text))
+    print("\t- content: ", end="")
+    print(r.text)
