5aba650c97b6bd18095ee624210e2d034b35ee2d
nadazkai21 <nadazaki019@gmail.com>
2024-05-22 04:18:43 +0300

    fixing 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,15 +6,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, 'utf8', (error, response, body) => {
-  if (error) {
-    console.error('Error making request:', error);
-  } else {
+  try {
     fs.writeFile(filePath, body, 'utf8', (err) => {
       if (err) {
-        console.error('Error writing file:', err);
-      } else {
-        console.log('File written successfully');
+        console.error(err);
       }
     });
+  } catch (error) {
+    console.error(error);
   }
 });
