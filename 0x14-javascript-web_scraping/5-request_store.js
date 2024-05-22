3e65293f5bb2aaf10a54ceb5b9c7d46c552fcd65
nadazkai21 <nadazaki019@gmail.com>
2024-05-22 04:21:29 +0300

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,6 +6,9 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, 'utf8', (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
   try {
     fs.writeFile(filePath, body, 'utf8', (err) => {
       if (err) {
